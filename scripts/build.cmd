67d80dfe3dd64a87290c17f32f87544b151e5a2e
Ethan Kletschke <bbruhingson@gmail.com>
2026-01-30 14:36:29 +0200

    fix: Allow build.cmd to run from the scripts folder

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,6 +1,6 @@
 @REM Command Prompt compile script
 
 @REM First run "set_env.cmd" from GnuCOBOL's root folder
-cobc -x -I src src/main.cbl -o bin/CobCash
+cobc -x -I ../src ../src/main.cbl -o ../bin/CobCash
 
-@REM Note: Run from root folder of project, e.g. ./scripts/Compile.cmd
+@REM Note: Run from the same folder as the script

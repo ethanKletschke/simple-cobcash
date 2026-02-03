fb3c10824f46298ab8ac95ae6a4926afa1a9a931
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-03 15:28:59 +0200

    build: change output directory from bin/ to dist/

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,6 +1,8 @@
 @REM Command Prompt compile script
 
 @REM First run "set_env.cmd" from GnuCOBOL's root folder
-cobc -x -I ../src ../src/main.cbl -o ../bin/CobCash -Wno-terminator
+
+@REM Build in the distribution folder
+cobc -x -I ../src ../src/main.cbl -o ../dist/CobCash -Wno-terminator
 
 @REM Note: Run from the "scripts" folder

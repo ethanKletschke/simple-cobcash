5c21cbb16def9a4b55888f7190d12e8db2d32c20
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-05 13:07:43 +0200

    fix: remove initialise statements
    Remove the INITIALISE statements that interfered
    with the program's values, e.g. the "=" divider
    on the receipt.

--- src/main.cbl
+++ src/main.cbl
@@ -37,9 +37,9 @@
            05 WS-Disp-Change PIC $$$$,$$9.99 VALUE 0.
       *Today's date, for use in the REPORT section.
        01  WS-Date PIC 9999/99/99.
-      *Miscellaneous data items for formatting
-       01  WS-Formatting.
-           05 WS-Equals-Divider PIC X(45) VALUE ALL "=".
+      *Miscellaneous data item for report formatting
+       01  WS-Equals-Divider.
+           05 FILLER PIC X(45) VALUE ALL "=".
       *Error fields
        01  WS-Error-Values.
            05 WS-Error-Msg PIC X(25) VALUE "Unspecified Error".
@@ -48,10 +48,10 @@
        REPORT SECTION.
        RD  Receipt-Report
            CONTROLS ARE FINAL
-           PAGE LIMIT IS 36 LINES
+           PAGE LIMIT IS 11 LINES
                HEADING 1
                FIRST DETAIL 5
-               LAST DETAIL 34.
+               LAST DETAIL 10.
        01  TYPE IS PAGE HEADING.
            05 LINE PLUS 1.
                10 COL 1 VALUE "CobCash Receipt".
@@ -65,13 +65,13 @@
               10 COL 25 PIC X(45) SOURCE WS-Holder-Name.
            05 LINE PLUS 1.
               10 COL 1 VALUE "Amt. Owed:".
-              10 COL 25 PIC $$$$,$$9.99 SOURCE WS-Owed.
+              10 COL 25 PIC $ZZZ,ZZ9.99 SOURCE WS-Owed.
            05 LINE PLUS 1.
               10 COL 1 VALUE "Amt. Paid:".
-              10 COL 25 PIC $$$$,$$9.99 SOURCE WS-Paid.
+              10 COL 25 PIC $ZZZ,ZZ9.99 SOURCE WS-Paid.
            05 LINE PLUS 1.
               10 COL 1 VALUE "Change:".
-              10 COL 25 PIC $$$$,$$9.99 SOURCE WS-Change.
+              10 COL 25 PIC $ZZZ,ZZ9.99 SOURCE WS-Change.
            05 LINE PLUS 2.
               10 COL 1 VALUE "Thank you for your patronage!".
 
@@ -83,12 +83,8 @@
       D COPY "DebugScreen.cpy".
 
        PROCEDURE DIVISION.
-           INITIALISE WS-Card-Data
-           INITIALISE WS-Date
-           INITIALISE WS-Calc-Finances WITH FILLER
-           INITIALISE WS-Disp-Finances WITH FILLER
-           INITIALISE WS-Formatting    WITH FILLER
-           INITIALISE WS-Error-Values  WITH FILLER.
+           INITIALISE WS-Card-Data.
+           INITIALISE WS-Date.
 
            MOVE FUNCTION CURRENT-DATE(1:8)
                TO WS-Date.

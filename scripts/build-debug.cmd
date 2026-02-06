b049f4f9a0151c52e0f681d4a07597b764449a6d
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-06 16:05:09 +0200

    build: Delete build-debug.cmd

--- scripts/build-debug.cmd
+++
@@ -1,6 +0,0 @@
-@REM Command Prompt debug build script
-
-@REM First run "set_env.cmd" from GnuCOBOL's root folder
-cobc -I ../src -x ../src/main.cbl -o ../bin/DEBUG -fdebugging-line
-
-@REM Note: Run from the "scripts" folder

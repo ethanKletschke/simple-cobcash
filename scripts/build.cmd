9b79d678ef5eebf44933ff376becbc0996bbe0f4
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-08 06:04:57 +0200

    build: change output directory in build.cmd

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -3,6 +3,6 @@
 @REM First run "set_env.cmd" from GnuCOBOL's root folder
 
 @REM Build in the distribution folder
-cobc -x -I ../src ../src/main.cbl -o ../dist/CobCash -Wno-terminator
+cobc -x -I ../src ../src/main.cbl -o ../bin/CobCash -Wno-terminator
 
 @REM Note: Run from the "scripts" folder

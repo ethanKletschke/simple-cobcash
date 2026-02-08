7247d3fa09aa4a213287fb5d09e4d099add3e631
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-08 06:06:03 +0200

    docs: Update comment in build.cmd

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,8 +1,7 @@
 @REM Command Prompt compile script
-
 @REM First run "set_env.cmd" from GnuCOBOL's root folder
 
-@REM Build in the distribution folder
+@REM Build in the binary folder
 cobc -x -I ../src ../src/main.cbl -o ../bin/CobCash -Wno-terminator
 
 @REM Note: Run from the "scripts" folder
